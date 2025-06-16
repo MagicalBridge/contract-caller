bb477b2d39d42b2a637a2e4545b61aa5f1acd776
louis <450793585@qq.com>
2025-06-16 23:52:07 +0800

    feat: 🎸 add new flags

--- cmd/contract-caller/cli.go
+++ cmd/contract-caller/cli.go
@@ -1,1 +1,49 @@
 package main
+
+import (
+	"context"
+
+	"github.com/urfave/cli/v2"
+
+	"github.com/MagicalBridge/contract-caller/common/cliapp"
+	flag2 "github.com/MagicalBridge/contract-caller/flags"
+)
+
+func runDappLinkVrf(ctx *cli.Context, shutdown context.CancelCauseFunc) (cliapp.Lifecycle, error) {
+	return nil, nil
+}
+
+func runMigrations(ctx *cli.Context) error {
+	return nil
+}
+
+func NewCli(GitCommit string, GitDate string) *cli.App {
+	flags := flag2.Flags
+	return &cli.App{
+		Version:              "v0.0.1",
+		Description:          "An indexer of all optimism events with a serving api layer",
+		EnableBashCompletion: true,
+		Commands: []*cli.Command{
+			{
+				Name:        "index",
+				Flags:       flags,
+				Description: "Runs the indexing service",
+				Action:      cliapp.LifecycleCmd(runDappLinkVrf),
+			},
+			{
+				Name:        "migrate",
+				Flags:       flags,
+				Description: "Runs the database migrations",
+				Action:      runMigrations,
+			},
+			{
+				Name:        "version",
+				Description: "print version",
+				Action: func(ctx *cli.Context) error {
+					cli.ShowVersion(ctx)
+					return nil
+				},
+			},
+		},
+	}
+}
--- cmd/contract-caller/main.go
+++ cmd/contract-caller/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"os"
+
+	"github.com/ethereum/go-ethereum/log"
+
+	"github.com/MagicalBridge/contract-caller/common/opio"
+)
+
+var (
+	GitCommit = ""
+	GitData   = ""
+)
 
 func main() {
-	fmt.Print("Hello, World!")
+	// set up logging
+	log.SetDefault(log.NewLogger(log.NewTerminalHandlerWithLevel(os.Stderr, log.LevelInfo, true)))
+	// create the cli app
+	app := NewCli(GitCommit, GitData)
+	// create a context with interrupt blocker
+	ctx := opio.WithInterruptBlocker(context.Background())
+	// run the cli app
+	if err := app.RunContext(ctx, os.Args); err != nil {
+		log.Error("Application failed", "error", err)
+		os.Exit(1)
+	}
 }
